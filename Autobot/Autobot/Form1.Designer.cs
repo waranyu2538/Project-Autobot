c9876f2b0c3cbe7ed7b2c85158b3cbe73523f0b2
waranyu2538 <5635512102@psu.ac.th>
2018-08-13 16:30:04 +0700

    Autobot

---
+++ Autobot/Autobot/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Autobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TName = new System.Windows.Forms.TextBox();
+            this.LStatus1 = new System.Windows.Forms.Label();
+            this.BStop = new System.Windows.Forms.Button();
+            this.BStart = new System.Windows.Forms.Button();
+            this.LStatus = new System.Windows.Forms.Label();
+            this.BGWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.GBox1 = new System.Windows.Forms.GroupBox();
+            this.LB2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.GBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ชื่อเครื่อง :";
+            // 
+            // TName
+            // 
+            this.TName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.TName.BackColor = System.Drawing.SystemColors.Info;
+            this.TName.Location = new System.Drawing.Point(98, 91);
+            this.TName.Name = "TName";
+            this.TName.Size = new System.Drawing.Size(94, 20);
+            this.TName.TabIndex = 9;
+            // 
+            // LStatus1
+            // 
+            this.LStatus1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LStatus1.AutoSize = true;
+            this.LStatus1.BackColor = System.Drawing.Color.White;
+            this.LStatus1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.LStatus1.ForeColor = System.Drawing.Color.Pink;
+            this.LStatus1.Location = new System.Drawing.Point(11, 54);
+            this.LStatus1.Name = "LStatus1";
+            this.LStatus1.Size = new System.Drawing.Size(0, 24);
+            this.LStatus1.TabIndex = 11;
+            // 
+            // BStop
+            // 
+            this.BStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BStop.Location = new System.Drawing.Point(115, 128);
+            this.BStop.Name = "BStop";
+            this.BStop.Size = new System.Drawing.Size(75, 23);
+            this.BStop.TabIndex = 8;
+            this.BStop.Text = "หยุดทำงาน";
+            this.BStop.UseVisualStyleBackColor = true;
+            this.BStop.Click += new System.EventHandler(this.BStop_Click);
+            // 
+            // BStart
+            // 
+            this.BStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.BStart.Location = new System.Drawing.Point(12, 128);
+            this.BStart.Name = "BStart";
+            this.BStart.Size = new System.Drawing.Size(75, 23);
+            this.BStart.TabIndex = 7;
+            this.BStart.Text = "เริ่มทำงาน";
+            this.BStart.UseVisualStyleBackColor = true;
+            this.BStart.Click += new System.EventHandler(this.BStart_Click);
+            // 
+            // LStatus
+            // 
+            this.LStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LStatus.AutoSize = true;
+            this.LStatus.BackColor = System.Drawing.Color.LavenderBlush;
+            this.LStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.LStatus.Location = new System.Drawing.Point(11, 18);
+            this.LStatus.Name = "LStatus";
+            this.LStatus.Size = new System.Drawing.Size(92, 26);
+            this.LStatus.TabIndex = 6;
+            this.LStatus.Text = "Status : ";
+            // 
+            // BGWorker1
+            // 
+            this.BGWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BGWorker1_DoWork);
+            // 
+            // GBox1
+            // 
+            this.GBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.GBox1.Controls.Add(this.radioButton2);
+            this.GBox1.Controls.Add(this.radioButton1);
+            this.GBox1.Controls.Add(this.LB2);
+            this.GBox1.Location = new System.Drawing.Point(203, 17);
+            this.GBox1.Name = "GBox1";
+            this.GBox1.Size = new System.Drawing.Size(131, 148);
+            this.GBox1.TabIndex = 12;
+            this.GBox1.TabStop = false;
+            // 
+            // LB2
+            // 
+            this.LB2.AutoSize = true;
+            this.LB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)), true);
+            this.LB2.Location = new System.Drawing.Point(20, 16);
+            this.LB2.Name = "LB2";
+            this.LB2.Size = new System.Drawing.Size(0, 15);
+            this.LB2.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 101);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(9, 120);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 170);
+            this.Controls.Add(this.GBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TName);
+            this.Controls.Add(this.LStatus1);
+            this.Controls.Add(this.BStop);
+            this.Controls.Add(this.BStart);
+            this.Controls.Add(this.LStatus);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.GBox1.ResumeLayout(false);
+            this.GBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TName;
+        private System.Windows.Forms.Label LStatus1;
+        private System.Windows.Forms.Button BStop;
+        private System.Windows.Forms.Button BStart;
+        private System.Windows.Forms.Label LStatus;
+        private System.ComponentModel.BackgroundWorker BGWorker1;
+        private System.Windows.Forms.GroupBox GBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label LB2;
+    }
+}
+
---
+++ Autobot/Autobot/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AutoItX3Lib;
+using System.Threading;
+
+
+namespace Autobot
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        AutoItX3 Autobot1 = new AutoItX3();
+
+        string Programe;
+        public int C1 = 0;
+        public int C2 = 0;
+        public int C3 = 0;
+        public int C4 = 0;
+        public int C5 = 0;
+        public int C6 = 0;
+        public int C7 = 0;
+
+        void Lc(string GameTitle, int X, int Y)
+        {
+            Autobot1.ControlClick(GameTitle, "", "", "LEFT", 1, X, Y);
+        }
+
+
+        bool EzSearch(int X, int Y, int Rect, int Color, int Shade)
+        {
+            Autobot1.PixelSearch(X - Rect, Y - Rect, X + Rect, Y + Rect, Color, Shade);
+
+            if (Autobot1.error == 0)
+            {
+                //Console.WriteLine("X = " + X + " :Y = " + Y + " :Color = 0x" + Color.ToString("X") + " / true");
+                return true;
+            }
+
+            // Console.WriteLine("X = " + X + " :Y = " + Y + " :Color = 0x" + Color.ToString("X") + " / false");
+            return false;
+        }
+
+        void Quest_Start()
+        {
+            if (!EzSearch(170, 410, 4, 0xFBFBFB, 4) || (!EzSearch(172, 410, 4, 0xFBFBFB, 4)))
+            //if (!EzSearch(75, 431, 4, 0x090909, 4) || (!EzSearch(316, 426, 4, 0x080808, 4)))
+            {
+                if (EzSearch(13, 202, 8, 0x6D2B81, 10) || (EzSearch(9, 206, 8, 0x9C3EB8, 10)))
+                //if (EzSearch(9, 195, 4, 0x8D38A6, 4) || (EzSearch(9, 197, 4, 0x8D38A6, 4)))
+                {
+                    if(EzSearch(8, 201, 4, 0x311639, 4) || (EzSearch(9, 202, 4, 0x8D38A6, 4)))
+                    //if (EzSearch(16, 198, 4, 0xB34CD3, 4) || (EzSearch(16, 199, 4, 0xAD48CB, 4)))
+                    {
+                        Console.WriteLine("step 1 QuestF");
+                        Lc(Programe, 49, 202);
+                        Thread.Sleep(1000);
+                    }
+                }
+                // else if (EzSearch(9, 239, 8, 0x8D38A6, 4) && (EzSearch(9, 241, 8, 0x8D38A6, 4)))
+                // {
+                // if (EzSearch(9, 242, 8, 0x8D38A6, 4) || (EzSearch(10, 244, 4, 0x5D256D, 4)))
+                //  {
+                //   Thread.Sleep(2000);
+                //   Console.WriteLine("step 1 QuestT");
+                //  Lc(Programe, 53, 249);
+                //  Thread.Sleep(1000);
+
+                //   Skip_Quest();
+                // }
+                //}
+            }
+        }
+
+        void Skip_Quest()
+        {
+            if (EzSearch(75, 431, 8, 0x050400, 4) || (EzSearch(316, 426, 8, 0x040301, 4) || (EzSearch(472, 419, 8, 0x040200, 4)) && (EzSearch(647, 92, 8, 0xFFFFFF, 4))))
+            {
+                if (EzSearch(36, 89, 8, 0xFFFFFF, 4) || (EzSearch(37, 96, 8, 0xFFFFFF, 4)))
+                {
+                    if (EzSearch(60, 93, 8, 0xFFFFFF, 4) || (EzSearch(75, 9, 8, 0xFFFFFF, 4)))
+                    {
+                        Console.WriteLine("step 2 Skips");
+                        Lc(Programe, 69, 93);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+
+        void Accept_Quest()
+          {
+              if (EzSearch(75, 431, 8, 0x050400, 4) || (EzSearch(316, 426, 8, 0x040301, 4) || (EzSearch(472, 419, 8, 0x040200, 4)) && (EzSearch(647, 92, 8, 0xFFFFFF, 4))))
+              //if (EzSearch(75, 431, 8, 0x050400, 4) && (EzSearch(316, 426, 8, 0x040301, 4) && (EzSearch(472, 419, 8, 0x040200, 4))))
+              {
+                  if (EzSearch(552, 404, 8, 0xFF7B50, 4) && (EzSearch(633, 404, 8, 0xFB794F, 4)))
+                  {
+                      Console.WriteLine("step 3 Accept");
+                      Lc(Programe, 601, 406);
+                      Thread.Sleep(1000);
+                  }
+              }
+          }
+
+          void Claim_Quest()
+          {
+              if(EzSearch(198, 90, 8, 0x515F6E, 4) && (EzSearch(445, 98, 8, 0x515F6E, 4)))
+              {
+                  if (EzSearch(282, 401, 8, 0xFF7B50, 4) || (EzSearch(278, 413, 8, 0xFF7B50, 4)))
+                  {
+                      if (EzSearch(394, 393, 8, 0xFF7B50, 4) || (EzSearch(395, 415, 8, 0xFF7B50, 4)))
+                      {
+                          Console.WriteLine("step 4 Claim");
+                          Lc(Programe, 337, 404);
+                          Thread.Sleep(1000);
+                      }
+                  }
+              }
+          }
+
+        private void BStart_Click(object sender, EventArgs e)
+        {
+            Autobot1.AutoItSetOption("CaretCoordMode", 2);
+            Autobot1.AutoItSetOption("MouseCoordMode", 2);
+            Autobot1.AutoItSetOption("PixelCoordMode", 2);
+
+            Programe = TName.Text;
+
+            Autobot1.WinGetHandle(Programe);
+            Autobot1.WinActivate(Programe);
+
+            Console.WriteLine(EzSearch(544, 234, 8, 0xF57E29, 4));
+
+
+           // if (Autobot1.WinActive(Programe) == 1)
+            //{
+                LStatus1.Text = Programe + " ทำงาน !!";
+                Autobot1.WinMove(Programe, "", 0, 0, 680, 480);
+
+                Console.WriteLine("เริ่มเเล้วนะ");
+
+                if (!BGWorker1.IsBusy)
+                    BGWorker1.RunWorkerAsync();
+
+                    LB2.Text = "เริ่มเเล้วนะ !!";
+           // }
+        }
+
+        private void BStop_Click(object sender, EventArgs e)
+        {
+            if (!BGWorker1.IsBusy)
+                BGWorker1.CancelAsync();
+
+            Console.WriteLine("หยุดอยู่");
+            LB2.Text = "หยุดเเล้วนะ !!";
+        }
+
+        private void BGWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Programe = TName.Text;
+            Autobot1.WinActive(Programe);
+
+            while (true)
+            {
+                while (true)
+                {
+                    Quest_Start();
+
+                    if (EzSearch(163, 348, 4, 0xFF7B50, 8) && (EzSearch(216, 354, 4, 0xFF7B50, 8)))
+                    {
+                        if (EzSearch(647, 92, 4, 0xFFFFFF, 8) && (EzSearch(649, 92, 4, 0xFFFFFF, 8)))
+                        {
+                            Console.WriteLine("Quest ซ้อน !!");
+                            Lc(Programe, 186, 350);
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    else
+                    Skip_Quest();
+
+                    Accept_Quest();
+
+                    if (EzSearch(163, 346, 4, 0x59B0A8, 8) && (EzSearch(216, 357, 4, 0x59B0A8, 8)))
+                    {
+                        if (EzSearch(647, 92, 4, 0xFFFFFF, 8) && (EzSearch(649, 92, 4, 0xFFFFFF, 8)))
+                        {
+                            Console.WriteLine("Accept Quest ซ้อน !!");
+                            Lc(Programe, 189, 351);
+                            Thread.Sleep(1000);
+                        }
+                    }
+
+                    Claim_Quest();
+                    break;
+                }
+            }
+            if (BGWorker1.CancellationPending == true)
+            {
+                e.Cancel = true;
+                return;
+            }
+        
+        
+        }
+
+        
+    }
+}
+//}
+
+
